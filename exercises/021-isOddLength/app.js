ec7102bdbb47319f2fdeed72dfd33a103e96e7ee
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-08-02 23:35:50 -0400

    Terminados hasta el ejercicio 31

--- exercises/021-isOddLength/app.js
+++ exercises/021-isOddLength/app.js
@@ -1,1 +1,8 @@
 // Write your function here
+const isOddLength = (word) => {
+    if (word.length % 2 != 0) return true
+    else return false
+}
+
+let output = isOddLength('special');
+console.log(output); // --> true
--- exercises/028-square/app.js
+++ exercises/028-square/app.js
@@ -1,1 +1,8 @@
 // Write your function here
+
+function square (num){
+    return Math.pow(num, 2)
+}
+
+let output = square(5);
+console.log(output); // --> 25
