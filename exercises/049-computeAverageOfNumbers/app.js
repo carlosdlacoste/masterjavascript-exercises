dbe82ac89218605754525db0551a2b84d340783f
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-08-31 19:42:00 -0400

    Terminados ejercicios hasta el 52

--- exercises/049-computeAverageOfNumbers/app.js
+++ exercises/049-computeAverageOfNumbers/app.js
@@ -1,1 +1,10 @@
 // Write your function here
+function  computeAverageOfNumbers(arr){
+    let sum = 0
+    for(let i = 0; i < arr.length; i++) sum += arr[i]
+    return sum/arr.length
+}
+
+let input = [1, 2, 3, 4, 5];
+let output = computeAverageOfNumbers(input);
+console.log(output); // --> 3
--- exercises/050-getNthElement/app.js
+++ exercises/050-getNthElement/app.js
@@ -1,4 +1,9 @@
 function getNthElement(array, n) {
   // Add your code after this line
-  
+  for(let i = 0; i < array.length; i++){
+    if(i === n) return array[i]
+  }
 }
+
+let output = getNthElement([1, 3, 5], 0);
+console.log(output); // --> 3
